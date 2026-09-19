ea1aca0b876ee1248a3bffe3b21db14f51d0edb5
kiopilot824-rgb <kiopilot824@gmail.com>
2026-09-19 06:43:07 +0330

    Create railway_start.sh

---
+++ railway_start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+
+ENV_FILE="/app/.env"
+: > "$ENV_FILE"
+
+while IFS= read -r line; do
+  case "$line" in
+    ''|'#'*) continue ;;
+  esac
+  name="${line%%=*}"
+  case "$name" in
+    ''|*[!A-Za-z0-9_]*|[0-9]*) continue ;;
+  esac
+  eval "is_set=\${$name+x}"
+  if [ "$is_set" = "x" ]; then
+    eval "value=\${$name}"
+    printf '%s=%s\n' "$name" "$value" >> "$ENV_FILE"
+  fi
+done < /app/.env.example
+
+exec python /app/start.py
+
